f67dab6049d88127c645ca70f998d2da44626fb7
Pravin Lokeshwar <plokeshwar@loyaltymethods.com>
2018-05-07 16:47:53 +0530

    Executing using 5 threads (Staging)

--- DocworksCmsQA/Properties/AssemblyInfo.cs
+++ DocworksCmsQA/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: LevelOfParallelism(2)]
+[assembly: LevelOfParallelism(5)]
