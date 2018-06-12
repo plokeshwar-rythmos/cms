1df8c88febd71f40411d3f78e45743a119c32e26
plokeshwar-rythmos <plokeshwar@rythmos.com>
2018-06-12 15:03:27 +0530

    Adding WebServices

---
+++ DocworksCmsQA/DockworksApi/CmsCommonMethods.cs
@@ -0,0 +1,71 @@
+﻿using DocWorksQA.DockworksApi;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocWorksQA.CmsApiMethods
+{
+    public class CmsCommonMethods
+    {
+        public static JObject createProject(WSAPIClient client, Dictionary<string, object> data, string token)
+        {
+            JObject c = (JObject)client.SendPost("api/Projects", data, token);
+            Assert.NotNull(c);
+            return c;
+        }
+
+
+        public JObject getProjects(WSAPIClient client, string token)
+
+        {
+            JObject c = (JObject)client.SendGet("api/Projects", token);
+            Assert.NotNull(c);
+            return c;
+        }
+
+        public static JObject getResponse(WSAPIClient client, String responseID, string token)
+        {
+            JObject c = (JObject)client.SendGet("api/Responses/" + responseID, token);
+            Assert.NotNull(c);
+            return c;
+        }
+
+
+        public static string getResponseCompleteExecution(WSAPIClient client, String responseID, string token)
+        {
+
+            string status = "";
+            for (int i = 0; i < 50; i++)
+            {
+                JObject r = CmsCommonMethods.getResponse(client, responseID, token);
+                Console.WriteLine(r);
+
+
+                status = r.GetValue("status").ToString();
+
+                Console.WriteLine("Current Status of Response ID (" + responseID + ") : " + status);
+                if (status.Equals("1"))
+                {
+                    Console.WriteLine("Current Status of Response ID (" + responseID + ") : " + status);
+                    System.Threading.Thread.Sleep(4000);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            return status;
+        }
+
+
+    
+
+
+
+    }
+}
